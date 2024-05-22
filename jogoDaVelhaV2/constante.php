61256cb9548cf863550b8579953a4db91f1d32ac
Edson Renan Baierle <109677007+edsonrenanbaierle@users.noreply.github.com>
2024-05-22 14:43:44 -0300

    Delete jogoDaVelhaV2 directory

--- jogoDaVelhaV2/constante.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define("ICON", ".");
-    define("PLAYER_ONE_ICON", 'x');
-    define("PLAYER_TWO_ICON", 'o');
-
-    
-?>
--- jogoDaVelhaV2/getPlayersName.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once __DIR__ . '/constante.php';
-
-    function getPlayersName() : array {
-        return [
-            readline("Player 1(" . PLAYER_ONE_ICON . ") digite o seu nome: "),
-            readline("Player 2(" . PLAYER_TWO_ICON . ") digite o seu nome: "),
-        ];
-    }
